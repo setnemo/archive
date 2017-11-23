1ba2b05639950eceff865be9fcbd8e5dc1a2b8dc
Artem PAKHOMOV <apakhomo@e2r5p5.unit.ua>
2017-11-23 09:58:49 +0200

    backup

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apakhomo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/25 09:41:04 by apakhomo          #+#    #+#             */
+/*   Updated: 2017/10/25 09:41:05 by apakhomo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		++i;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (argv[i] != '\0')
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			i++;
+		}
+	}
+	return (0);
+}
