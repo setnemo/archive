c1832477cba001cfffa261b731d22ebbf067bdd4
Artem PAKHOMOV <apakhomo@e1r1p15.unit.ua>
2018-02-14 10:43:14 +0200

    new algo

--- srcs/start.c
+++ srcs/start.c
@@ -15,13 +15,17 @@
 void	check_map(t_fill *game)
 {
 	char	*line;
+	char	*temp;
 
-	get_next_line(STDIN_FILENO, &line);
-	if (ft_strstr(line, "==") != NULL)
+	if (!(get_next_line(STDIN_FILENO, &line)))
 	{
-		// ft_strdel(&line);
+		// if (game->map)
+		// 	ft_free_arr(game->map);
+		// if (game->bit)
+		// 	ft_free_arr(game->bit);
 		exit(0);
 	}
+	temp = line;
 	dprintf(game->fd,"%s\n", line);
 	line = line + 8;
 	game->map_size[0] = ft_atoi(line);
@@ -29,17 +33,19 @@ void	check_map(t_fill *game)
 		line++;
 	game->map_size[1] = ft_atoi(line);
 	dprintf(game->fd,"Map Size:[%i][%i]\n", game->map_size[0], game->map_size[1]);
-	// ft_strdel(&line);
+	ft_strdel(&temp);
 	get_next_line(STDIN_FILENO, &line);
-	// ft_strdel(&line);
+	ft_strdel(&line);
 }
 
 void		read_bit(t_fill *game)
 {
 	int		a;
 	char	*line;
+	char	*temp;
 
 	get_next_line(STDIN_FILENO, &line);
+	temp = line;
 	line += 6;
 	game->bit_size[0] = ft_atoi(line);
 	while (*line != 32)
@@ -50,11 +56,12 @@ void		read_bit(t_fill *game)
 	game->bit = (char**)malloc((sizeof(char*) * game->bit_size[0]) + 1);
 	game->bit[game->bit_size[0]] = NULL;
 	a = 0;
+	ft_strdel(&temp);
 	while (a < game->bit_size[0])
 	{
 		get_next_line(STDIN_FILENO, &line);
 		game->bit[a] = ft_strdup(line);
-		//ft_strdel(&line);
+		ft_strdel(&line);
 		a++;
 	}
 }
@@ -99,6 +106,8 @@ void	start_play(t_fill *game)
 		dprintf(game->fd, "My bit size:[%i][%i]\n", game->bit_size[0], game->bit_size[1]);
 		spot_loc(game);
 	}
+	system("leaks -quiet filler > 2");
+
 	while (1)
 	{
 		read_map(game);
@@ -115,6 +124,7 @@ void	start_filler(void)
 
 	game = (t_fill*)malloc(sizeof(t_fill));
 	ft_bzero(game, sizeof(t_fill));
+	game->a_loc = 0;
 	game->fd = open("log", O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	dprintf(game->fd, "test\n");
 	get_next_line(STDIN_FILENO, &line);
