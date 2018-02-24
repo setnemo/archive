18f2ab205bf6d952cd5d456aaefc49ac9a7b5658
Artem PAKHOMOV <apakhomo@e1r1p8.unit.ua>
2018-02-24 14:19:20 +0200

    start legends

--- srcs/start_fdf.c
+++ srcs/start_fdf.c
@@ -19,23 +19,6 @@ void	restart(t_mlx *data)
 	start_fdf(data);
 }
 
-// void	z(t_mlx *data, int flag)
-// {
-// 	t_map *lines;
-
-// 	lines = data->line;
-// 	while (lines)
-// 	{
-// 		lines->pz = (flag == 0) ? (lines->pz += 1) : (lines->pz -= 1);
-// 		printf("test->pz:%f\n", lines->pz);
-// 		if (lines->next)
-// 			lines = lines->next;
-// 		else
-// 			break ;
-// 	}
-// 	rotate_fdf(data, 3);
-// }
-
 void	colorize(t_mlx *data, int flag)
 {
 	t_map *lines;
@@ -62,52 +45,13 @@ void	colorize(t_mlx *data, int flag)
 	}
 }
 
+
 void	exit_free(t_mlx *data)
 {
 	free_data(data, 0);
 	system("leaks -quiet fdf");
 	exit(0);
 }
-
-// int		deal_key(int k, t_mlx *data)
-// {
-// 	if (k == 65307)
-// 		exit_free(data);
-// 	if (k == 122)
-// 	{
-// 		data->radx += 0.017452;
-// 		rotate_fdf(data, 0);
-// 	}
-// 	if (k == 120)
-// 	{
-// 		data->radx -= 0.017452;
-// 		rotate_fdf(data, 0);
-// 	}
-// 	if (k == 99)
-// 	{
-// 		data->rady += 0.017452;
-// 		rotate_fdf(data, 1);
-// 	}
-// 	if (k == 118)
-// 	{
-// 		data->rady -= 0.017452;
-// 		rotate_fdf(data, 1);
-// 	}
-// 	if (k == 98)
-// 	{
-// 		data->radz += 0.017452;
-// 		rotate_fdf(data, 2);
-// 	}
-// 	if (k == 110)
-// 	{
-// 		data->radz -= 0.017452;
-// 		rotate_fdf(data, 2);
-// 	}
-// 	ft_testintstr(k, "key");
-// 	if (k == 53 || (k >= 99 && k <= 126) )
-// 		restart(data);
-// 	return (k);
-// }
 
 int		deal_key(int k, t_mlx *data)
 {
@@ -133,16 +77,6 @@ int		deal_key(int k, t_mlx *data)
 		data->rady -= 0.0125;
 		rotate_fdf(data, 1);
 	}
-	if (k == 43)
-	{
-		data->radz += 0.0125;
-		rotate_fdf(data, 2);
-	}
-	if (k == 47)
-	{
-		data->radz -= 0.0125;
-		rotate_fdf(data, 2);
-	}
 	if (k == 24)
 		scale(data, 1);
 	if (k == 27)
@@ -155,10 +89,6 @@ int		deal_key(int k, t_mlx *data)
 		data->move = 3;
 	if (k == 88)
 		data->move = 1;
-	// if (k == 6)
-	// 	z(data, 0);
-	// if (k == 7)
-	// 	z(data, 1);
 	if (k == 8)
 	{
 		if (data->coloriz == 1)
@@ -167,54 +97,17 @@ int		deal_key(int k, t_mlx *data)
 			data->coloriz = 1;
 		colorize(data, data->coloriz);
 	}
-	// if (k == 25)
-	// 	z(data, 1);
-
+	if (k == 1)
+	{
+		data->radx = 0;
+		data->rady = 0;
+		data->move = 0;
+	}
 	ft_testintstr(k, "key");
-	if (k >= 6 && k <= 126)
+	if (k >= 1 && k <= 126)
 		restart(data);
 	return (k);
 }
-
-// int		deal_key(int k, t_mlx *data)
-// {
-// 	if (k == 53)
-// 		exit_free(data);
-// 	if (k == 123)
-// 	{
-// 		data->radx += 0.08;
-// 		rotate_fdf(data, 0, 0);
-// 	}
-// 	if (k == 124)
-// 	{
-// 		data->radx -= 0.017452;
-// 		rotate_fdf(data, 0, 1);
-// 	}
-// 	if (k == 125)
-// 	{
-// 		data->rady += 0.017452;
-// 		rotate_fdf(data, 1, 0);
-// 	}
-// 	if (k == 126)
-// 	{
-// 		data->rady -= 0.017452;
-// 		rotate_fdf(data, 1, 1);
-// 	}
-// 	if (k == 83)
-// 	{
-// 		data->radz -= 0.017452;
-// 		rotate_fdf(data, 2, 0);
-// 	}
-// 	if (k == 85)
-// 	{
-// 		data->radz -= 0.017452;
-// 		rotate_fdf(data, 2, 1);
-// 	}
-// 	ft_testintstr(k, "key");
-// 	if (k == 53 || (k >= 123 && k <= 126) || k == 83 || k == 85)
-// 		restart(data);
-// 	return (k);
-// }
 
 void			scale(t_mlx *data, int flag)
 {
@@ -330,10 +223,3 @@ void				start_fdf(t_mlx *data)
 		y++;
 	}
 }
-
-		// else
-		// 	y = 0;
-	//	printf(":ШШШ:lines->px lines->py lines->px1 lines->py1 lines->px2 lines->py2 lines->pz lines->pc\n");
-		//printf("%f		%f		%f		%f		%f		%f\n",lines->px, lines->py,lines->px1, lines->py1,lines->px2, lines->py2);
-//		printf(" %i         %f %f %f %d         %f       %f         %f         %f          %f        %f        %lu\n",i, lines->px, lines->py,lines->px1, lines->py1,lines->px2, lines->py2,lines->pz,lines->pc);
-		//i++;
