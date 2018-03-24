4ff85f86437ee9366a872a3e358e71c96e50f17c
Artem Pakhomov <setnemo@gmail.com>
2018-03-24 09:07:35 +0200

    bfs save path

--- srcs/find_ways.c
+++ srcs/find_ways.c
@@ -12,50 +12,95 @@
 
 #include "lemin.h"
 
-static void		write_path_new(t_lem *data, t_way *ways, int i)
+static void		add_new_path(t_lem *data, int i, int j, int *copy)
 {
-	if (data && ways && i)
-		;
+	int **path;
 	int a;
 	int b;
-	int r;
 
-	r = 0;
+	path = data->way->path;
 	a = 0;
-	while (ways->path[a])
+	while (path[a])
+		a++;
+	ft_printf("\n	while (path[a])\n");
+	path[a] = (int*)malloc(sizeof(int) * data->how_rooms + 1);
+	ft_memset(path[a], -1, (sizeof(int) * data->how_rooms + 1));
+	b = 0;
+	ft_printf("\nwhile (copy[b]\n");
+	while (copy[b] != i)
 	{
-		b = 0;
-		while (ways->path[a][b])
+		path[a][b] = copy[b];
+		b++;
+	}
+	ft_printf("\nwhile (copy[b] != -1)\n");
+	path[a][b] = i;
+	path[a][b + 1] = j;
+	ft_printf("\naddnew done\n");
+}
+
+static int		search_room_in_path(int *path, int i, int j, int stop)
+{
+	int a;
+	int flag;
+
+	a = 0;
+	flag = 0;
+	if (path == NULL)
+		return (0);
+	while (a < stop)
+	{
+		if (path[a] == i)
 		{
-			if (ways->path[a][b] == -1)
+			if (path[a + 1] == -1)
 			{
-				r = 1;
-				break ;
+				flag = 1;
+				path[a + 1] = j;
 			}
-			b++;
+			else if (path[a + 1] != j)
+				return (-1);
+			else
+				return (0);
 		}
-		if (r)
-			break ;
 		a++;
 	}
-	ways->path[a][b] = i;
+	return (flag ? 1 : 0);
 }
 
-static void		write_path_current(t_lem *data, t_way *ways, int i)
+static void		write_path(t_lem *data, int i, int j)
 {
-	if (data && ways && i)
-		;
+	int a;
+	int b;
+	int addnew;
+	int flag;
+	int **allpath;
 
-}
-
-
-static void		write_path(t_lem *data, int i, int flag)
-{
-	if (flag)
-		write_path_current(data, data->way, i);
-	else
+	a = 0;
+	allpath = data->way->path;
+	flag = 0;
+	addnew = 0;
+	while (a < data->how_rooms)
 	{
-		write_path_new(data, data->way, i);
+		while ((addnew = search_room_in_path(allpath[a], i, j, data->how_rooms)))
+		{
+			ft_printf("\nsearch_room_in_path\n");
+			if (addnew == -1){
+				add_new_path(data, i, j, allpath[a]);
+			}
+			flag++;
+			a++;	
+		}
+		a++;
+	}
+	if (flag == 0)
+	{
+		b = 0;
+		ft_printf("\nflag == 0\n");
+		while (allpath[b])
+			b++;
+		allpath[b] = (int*)malloc(sizeof(int) * data->how_rooms + 1);
+		ft_memset(allpath[b], -1, (sizeof(int) * data->how_rooms + 1));
+		allpath[b][0] = i;
+		allpath[b][1] = j;
 	}
 }
 
@@ -63,7 +108,7 @@ static int		new_room(int a, int *buf)
 {
 	int i;
 
-i =0;
+	i = 0;
 	while (buf[i] != -1)
 	{
 		if (buf[i] == a)
@@ -99,7 +144,7 @@ static void		bfs(t_lem *data, int i, int c, int *buf)
 			}
 			if (data->links[i][a] == 1 && a == data->end_room)
 			{
-				write_path(data, a, 2);
+				write_path(data, i, a);
 				ft_printf("[%s] <<<<<", data->name_room[i]);
 			}
 			a++;
@@ -112,17 +157,17 @@ static void		bfs(t_lem *data, int i, int c, int *buf)
 		c++;
 	}
 	ft_printf(":THE_END:\n");
-	for (int x = 0; x < data->how_rooms + 1; ++x)
-	{
-		if (buf[x] != -1)
-			ft_printf("[%s] ", data->name_room[buf[x]]);
-		else
-		{
-			ft_printf("[ ] ");
-			break ;
-		}
-	}
-	ft_printf("\n");
+	// for (int x = 0; x < data->how_rooms + 1; ++x)
+	// {
+	// 	if (buf[x] != -1)
+	// 		ft_printf("[%s] ", data->name_room[buf[x]]);
+	// 	else
+	// 	{
+	// 		ft_printf("[ ] ");
+	// 		break ;
+	// 	}
+	// }
+	// ft_printf("\n");
 }
 
 void			find_way(t_lem *data)
@@ -143,4 +188,10 @@ void			find_way(t_lem *data)
 	ft_printf("data->end_room [%i]\n", data->end_room);
 	ft_printf("data->how_rooms [%i]\n", data->how_rooms);
 	bfs(data, data->start_room, 0, buf);
+	for (int k = 0; ways->path[k]; ++k)
+	{
+		for (int m = 0; data->name_room[ways->path[k][m]]; ++m)
+			ft_printf(" [%s] ", data->name_room[ways->path[k][m]]);
+		ft_printf("\n");
+	}
 }
