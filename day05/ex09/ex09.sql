7a95dd2edf5949efac027dab40267c242ae9a1ad
Artem Pakhomov <setnemo@gmail.com>
2018-04-09 15:58:38 +0300

    d05ex10

---
+++ day05/ex09/ex09.sql
@@ -0,0 +1,1 @@
+SELECT count(*) 'nb_short-films' FROM film WHERE duration <= 42;
---
+++ day05/ex10/ex10.sql
@@ -0,0 +1,1 @@
+SELECT title 'Title', summary 'Summary', prod_year FROM film, genre WHERE film.id_genre = genre.id_genre AND genre.name = 'erotic' ORDER BY prod_year DESC;
