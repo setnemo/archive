d8cedb6acfec83009720ca2c3c771c48771dd52e
Artem Pakhomov <setnemo@gmail.com>
2018-04-09 15:24:21 +0300

    d05ex03

--- day05/ex01/ex01.sql
+++ day05/ex01/ex01.sql
@@ -1,1 +1,1 @@
-CREATE TABLE ft_table (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, login VARCHAR(11) DEFAULT 'toto' NOT NULL, size ENUM('staff', 'student', 'other') NOT NULL, creation_date DATE NOT NULL);
+CREATE TABLE ft_table (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, `login` VARCHAR(8) DEFAULT 'toto' NOT NULL, `group` ENUM('staff', 'student', 'other') NOT NULL, `creation_date` DATE NOT NULL);
---
+++ day05/ex03/ex03.sql
@@ -0,0 +1,1 @@
+INSERT INTO ft_table (`login`, `creation_date`, `group`) SELECT last_name, birthdate, 'other' FROM user_card WHERE last_name LIKE "%a%" AND LENGTH(last_name) < 9 ORDER BY last_name LIMIT 10;
