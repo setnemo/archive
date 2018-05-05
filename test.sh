7f4f3807b232651728903e1b37bb1bc1bd0bba10
Artem Pakhomov <setnemo@gmail.com>
2018-05-05 12:57:45 +0300

    before norm

--- test.sh
+++ test.sh
@@ -100,112 +100,112 @@ diff -s example/championships/2014/sbuono/Asombra.s ./Asombra.s
 diff -s example/championships/2014/srabah-m/littlepuppy.s ./littlepuppy.s
 ./asm example/championships/2014/tdemay/champ.s
 ./orig_asm example/championships/2014/tdemay/champ.s
-diff -s 
+diff -s example/championships/2014/tdemay/champ.s ./champ.s
 ./asm example/championships/2014/vrey/darksasuke.s
 ./orig_asm example/championships/2014/vrey/darksasuke.s
-diff -s 
+diff -s example/championships/2014/vrey/darksasuke.s ./darksasuke.s
 ./asm example/championships/2014/ybouvet/casimir.s
 ./orig_asm example/championships/2014/ybouvet/casimir.s
-diff -s 
+diff -s example/championships/2014/ybouvet/casimir.s ./casimir.s
 ./asm example/championships/2017/adenis/Explosive_Kitty.s
 ./orig_asm example/championships/2017/adenis/Explosive_Kitty.s
-diff -s 
+diff -s example/championships/2017/adenis/Explosive_Kitty.s ./Explosive_Kitty.s
 ./asm example/championships/2017/agregoir/Kittystrophic.s
 ./orig_asm example/championships/2017/agregoir/Kittystrophic.s
-diff -s 
+diff -s example/championships/2017/agregoir/Kittystrophic.s ./Kittystrophic.s
 ./asm example/championships/2017/gnebie/ultima.s
 ./orig_asm example/championships/2017/gnebie/ultima.s
-diff -s 
+diff -s example/championships/2017/gnebie/ultima.s ./ultima.s
 ./asm example/championships/2017/iburel/gateau.s
 ./orig_asm example/championships/2017/iburel/gateau.s
-diff -s 
+diff -s example/championships/2017/iburel/gateau.s ./gateau.s
 ./asm example/championships/2017/jaustry/mise_a_jour_windows95.s
 ./orig_asm example/championships/2017/jaustry/mise_a_jour_windows95.s
-diff -s 
+diff -s example/championships/2017/jaustry/mise_a_jour_windows95.s ./mise_a_jour_windows95.s
 ./asm example/championships/2017/nolivier/run_Kitty_RUN.s
 ./orig_asm example/championships/2017/nolivier/run_Kitty_RUN.s
-diff -s 
+diff -s example/championships/2017/nolivier/run_Kitty_RUN.s ./run_Kitty_RUN.s
 ./asm example/championships/2017/pzarmehr/overwatch.s
 ./orig_asm example/championships/2017/pzarmehr/overwatch.s
-diff -s 
+diff -s example/championships/2017/pzarmehr/overwatch.s ./overwatch.s
 ./asm example/championships/2017/tbillard/Douceur_power.s
 ./orig_asm example/championships/2017/tbillard/Douceur_power.s
-diff -s 
+diff -s example/championships/2017/tbillard/Douceur_power.s ./Douceur_power.s
 ./asm example/Death.s
 ./orig_asm example/Death.s
-diff -s 
+diff -s example/Death.s ./Death.s
 ./asm example/ex.s
 ./orig_asm example/ex.s
-diff -s 
+diff -s example/ex.s ./ex.s
 ./asm example/examples/bee_gees.s
 ./orig_asm example/examples/bee_gees.s
-diff -s 
+diff -s example/examples/bee_gees.s ./examples/bee_gees.s
 ./asm example/examples/bigzork.s
 ./orig_asm example/examples/bigzork.s
-diff -s 
+diff -s example/examples/bigzork.s ./examples/bigzork.s
 ./asm example/examples/fluttershy.s
 ./orig_asm example/examples/fluttershy.s
-diff -s 
+diff -s example/examples/fluttershy.s ./examples/fluttershy.s
 ./asm example/examples/helltrain.s
 ./orig_asm example/examples/helltrain.s
-diff -s 
+diff -s example/examples/helltrain.s ./examples/helltrain.s
 ./asm example/examples/turtle.s
 ./orig_asm example/examples/turtle.s
-diff -s 
+diff -s example/examples/turtle.s ./examples/turtle.s
 ./asm example/examples/zor.s
 ./orig_asm example/examples/zor.s
-diff -s 
+diff -s example/examples/zor.s ./examples/zor.s
 ./asm example/examples/zork.s
 ./orig_asm example/examples/zork.s
-diff -s 
+diff -s example/examples/zork.s ./examples/zork.s
 ./asm example/Gagnant.s
 ./orig_asm example/Gagnant.s
-diff -s 
+diff -s example/Gagnant.s ./Gagnant.s
 ./asm example/jumper.s
 ./orig_asm example/jumper.s
-diff -s 
+diff -s example/jumper.s ./jumper.s
 ./asm example/lde.s
 ./orig_asm example/lde.s
-diff -s 
+diff -s example/lde.s ./lde.s
 ./asm example/leeloo.s
 ./orig_asm example/leeloo.s
-diff -s 
+diff -s example/leeloo.s ./leeloo.s
 ./asm example/mat.s
 ./orig_asm example/mat.s
-diff -s 
+diff -s example/mat.s ./mat.s
 ./asm example/maxidef.s
 ./orig_asm example/maxidef.s
-diff -s 
+diff -s example/maxidef.s ./maxidef.s
 ./asm example/mortel.s
 ./orig_asm example/mortel.s
-diff -s 
+diff -s example/mortel.s ./mortel.s
 ./asm example/new.s
 ./orig_asm example/new.s
-diff -s 
+diff -s example/new.s ./new.s
 ./asm example/Octobre_Rouge_V4.2.s
 ./orig_asm example/Octobre_Rouge_V4.2.s
-diff -s 
+diff -s example/Octobre_Rouge_V4.2.s ./Octobre_Rouge_V4.2.s
 ./asm example/sebc.s
 ./orig_asm example/sebc.s
-diff -s 
+diff -s example/sebc.s ./sebc.s
 ./asm example/slider2.s
 ./orig_asm example/slider2.s
-diff -s 
+diff -s example/slider2.s ./slider2.s
 ./asm example/Survivor.s
 ./orig_asm example/Survivor.s
-diff -s 
+diff -s example/Survivor.s ./Survivor.s
 ./asm example/tdc2.s
 ./orig_asm example/tdc2.s
-diff -s 
+diff -s example/tdc2.s ./tdc2.s
 ./asm example/tdc3.s
 ./orig_asm example/tdc3.s
-diff -s 
+diff -s example/tdc3.s ./tdc3.s
 ./asm example/tdc4.s
 ./orig_asm example/tdc4.s
-diff -s 
+diff -s example/tdc4.s ./tdc4.s
 ./asm example/Torpille.s
 ./orig_asm example/Torpille.s
-diff -s 
+diff -s example/Torpille.s ./Torpille.s
 ./asm example/toto.s
 ./orig_asm example/toto.s
-diff -s 
+diff -s example/toto.s ./toto.s
