9eb8bf07d330084e2c7cbc7c601395320cfe022d
Artem Pakhomov <setnemo@gmail.com>
2018-06-09 16:32:33 +0300

    Initial commit

---
+++ libft/src/ft_strcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apakhomo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/11 13:16:57 by apakhomo          #+#    #+#             */
+/*   Updated: 2017/11/11 13:16:57 by apakhomo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int			ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned char *ss1;
+	unsigned char *ss2;
+
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
+	while (*ss1 && *ss1 == *ss2)
+	{
+		ss1++;
+		ss2++;
+	}
+	return (*ss1 - *ss2);
+}
