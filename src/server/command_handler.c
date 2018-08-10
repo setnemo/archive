44a3b6b16f82c51fe7bb63ad9bb8020f9b7cb901
vilkoz <vilko.a@gmail.com>
2018-08-10 21:35:38 +0300

    add command logging

--- src/server/command_handler.c
+++ src/server/command_handler.c
@@ -1,5 +1,11 @@
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
 #include "server/command_callbacks.h"
 
 static char		*g_commands[] = {
@@ -11,15 +17,37 @@ static char		*(*g_command_callbacks[])(void *) = {
 	hello_test_callback
 };
 
-char	*command_handler(char *command)
+static void		write_log(const char *received, const char *responded)
+{
+	int		fd;
+
+	fd = open("command_log.csv", O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd < 0)
+	{
+		perror("Unable to open command_log.csv");
+		return ;
+	}
+	dprintf(fd, "\"%d\",\"%s\",\"%s\"\n", (int)time(NULL), received, responded);
+	close(fd);
+}
+
+char			*command_handler(char *command)
 {
 	int			i;
+	char		*responce;
 
 	i = -1;
+	responce = NULL;
 	while (g_commands[++i])
 	{
 		if (strncmp(command, g_commands[i], strlen(g_commands[i])) == 0)
-			return g_command_callbacks[i](command);
+		{
+			responce = g_command_callbacks[i](command);
+			break ;
+		}
 	}
-	return (strdup("Command not found!"));
+	if (responce == NULL)
+		responce = strdup("Command not found!");
+	write_log(command, responce);
+	return (responce);
 }
