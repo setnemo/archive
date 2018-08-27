1502a39a9a3dd6fb4178ce01fb6b71a37cacef32
Dana Mulish <dmulish@e1r12p2.unit.ua>
2018-08-27 18:55:58 +0300

    recognize_from_mic as external lib, kek

--- Makefile
+++ Makefile
@@ -20,6 +20,13 @@ SAM_LFLAGS = `sdl-config --libs`
 
 CJSON_LIB=external/cJSON/libcjson.a
 
+SPHINX_LIB = external/sphinx/libsphinx.a
+
+SPHINX_FLAGS = -Wall -Wextra -Werror -I include -g \
+			   `pkg-config --cflags pocketsphinx sphinxbase` \
+			   -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" \
+			   -DDICTDIR=\"$(shell pwd)/src/client/voice_recognition/\"
+
 CONFIG_FILE=include/server/config.h
 
 VPATH=src
@@ -60,15 +67,18 @@ $(SAM_LIB):
 $(CJSON_LIB):
 	make -C $(shell dirname $@)
 
+$(SPHINX_LIB):
+	make -C $(shell dirname $@)
+
 $(shell dirname $(CONFIG_FILE))/.env: $(shell dirname $(CONFIG_FILE))/.env.example
 	cp $< $@
 
 $(CONFIG_FILE): $(shell dirname $(CONFIG_FILE))/.env
 	cd $(shell dirname $@); /bin/bash ./$(shell basename $@).gen.sh
 
-$(CLIENT): $(CLIENT_BINS) $(SAM_LIB)
-	$(CC) $(FLAGS) -o $(CLIENT) $(CLIENT_BINS) $(SAM_LIB)\
-		$(SAM_LFLAGS)  $(CLIENT_LINKER_FLAGS)
+$(CLIENT): $(CLIENT_BINS) $(SAM_LIB) $(SPHINX_LIB)
+	$(CC) $(FLAGS) -o $(CLIENT) $(CLIENT_BINS) $(SAM_LIB) $(SPHINX_LIB)\
+		$(SAM_LFLAGS) $(SPHINX_FLAGS) $(CLIENT_LINKER_FLAGS)
 
 $(SERVER): $(CONFIG_FILE) $(SERVER_BINS) $(CJSON_LIB)
 	$(CC) $(FLAGS) -o $(SERVER) $(SERVER_BINS) $(CJSON_LIB) $(SERVER_LINKER_FLAGS)
@@ -77,10 +87,12 @@ clean:
 	/bin/rm -f $(CLIENT_BINS) $(SERVER_BINS)
 	make -C $(shell dirname $(SAM_LIB)) clean
 	make -C $(shell dirname $(CJSON_LIB)) clean
+	make -C $(shell dirname $(SPHINX_LIB)) clean
 
 fclean: clean
 	/bin/rm -f $(CLIENT) $(SERVER) $(CONFIG_FILE)
 	make -C $(shell dirname $(SAM_LIB)) fclean
+	make -C $(shell dirname $(SPHINX_LIB)) fclean
 
 re: fclean all
 
--- src/client/voice_recognition/voice_recognition.c
+++ src/client/voice_recognition/voice_recognition.c
@@ -1,75 +1,29 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sphinxbase/ad.h>
-#include <sphinxbase/err.h>
-#include "pocketsphinx.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   voice_recognition.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmulish <dmulish@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 19:04:02 by dmulish           #+#    #+#             */
+/*   Updated: 2018/08/27 18:54:11 by dmulish          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static void	sleep_msec(int32 ms)
-{
-	struct timeval tmo;
+#include "external/recognize_from_mic.h"
 
-	tmo.tv_sec = 0;
-	tmo.tv_usec = ms * 1000;
-	select(0, NULL, NULL, NULL, &tmo);
-}
-static char	*recognize_from_mic(cmd_ln_t *config, ps_decoder_t *ps)
-{
-	ad_rec_t	*ad;
-	uint8		utt_started;
-	uint8		in_speech;
-	int16		adbuf[2048];
-	int32		k;
-	char const	*hyp;
-
-	utt_started = FALSE;
-	ad = ad_open_sps((int)cmd_ln_float32_r(config, "-samprate"));
-	if (ad_start_rec(ad) < 0)
-		E_FATAL("Failed to start recording\n");
-	if (ps_start_utt(ps) < 0)
-		E_FATAL("Failed to start utterance\n");
-	while (1)
-	{
-		k = ad_read(ad, adbuf, 2048);
-		if (k < 0)
-			E_FATAL("Failed to read audio\n");
-		ps_process_raw(ps, adbuf, k, FALSE, FALSE);
-		in_speech = ps_get_in_speech(ps);
-		if (in_speech && !utt_started)
-		{
-			puts("Listening...");
-			utt_started = TRUE;
-		}
-		sleep_msec(100);
-		if (!in_speech && utt_started)
-		{
-			ps_end_utt(ps);
-			hyp = ps_get_hyp(ps, NULL);
-			if (hyp && hyp[0])
-			{
-				puts(hyp);
-				break ;
-			}
-			if (ps_start_utt(ps) < 0)
-				E_FATAL("Failed to start utterance\n");
-			utt_started = FALSE;
-		}
-	}
-	ad_close(ad);
-	return (strdup(hyp));
-}
-
-char	*get_user_speech(void)
+char		*get_user_speech(void)
 {
 	char			*result;
 	ps_decoder_t	*ps;
 	cmd_ln_t		*config;
 
 	config = cmd_ln_init(NULL, ps_args(), TRUE,
-	"-hmm", DICTDIR "en-us-adapt",
-	"-lm", MODELDIR "/en-us/en-us.lm.bin",	// custom language model (file must be present)
-	"-dict", DICTDIR "dict.dic",	// custom dictionary (file must be present)
-	"-logfn", "/dev/null",
-	"-mllr", DICTDIR "mllr_matrix", NULL);
+			"-hmm", DICTDIR "en-us-adapt",
+			"-lm", MODELDIR "/en-us/en-us.lm.bin",
+			"-dict", DICTDIR "dict.dic",
+			"-logfn", "/dev/null", NULL);
+			/*"-mllr", DICTDIR "mllr_matrix",*/
 	if (config == NULL)
 	{
 		fprintf(stderr, "Failed to create config object\n");
