5da625ab59c78d66a85970dfdff1c7de1edb24e9
Kostyantyn Shcherbyna <kshcherb@e1r11p4.unit.ua>
2018-09-04 21:03:03 +0300

    small talk rdy. norm OK

---
+++ include/server/small_talk.h
@@ -0,0 +1,12 @@
+#ifndef SMALL_TALK_H
+# define SMALL_TALK_H
+
+char	*flip_a_coin(void);
+char	*crystal_ball(void);
+char	*roll_the_dice(void);
+char	*sing_song(void);
+char	*tell_me_something(void);
+char	*tell_a_joke(void);
+char	*small_talk(char *str);
+
+#endif
