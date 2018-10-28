d950c6045a42e70c9b784ea894c3ab30f27a622c
Artem Pakhomov <setnemo@gmail.com>
2018-10-28 19:10:07 +0200

    search boundary done

--- app/index.php
+++ app/index.php
@@ -1,24 +1,24 @@
 <!doctype html>
 <html lang="ru">
-		<head>
-				<!-- Required meta tags -->
-				<meta charset="utf-8">
-				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-				<link rel="shortcut icon" type="image/x-icon"  href="/icon_uf.png" >
-				<!-- Bootstrap CSS -->
-				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-				<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>
-				<link rel="stylesheet" type="text/css" href="style.css"/>
-				<title>Rage Against The aPi</title>
-				<!-- Optional JavaScript -->
-				<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-				<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-				<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
-				<script type="text/javascript" src="script.js"></script>
-		</head>
-		<body>
+        <head>
+                <!-- Required meta tags -->
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <link rel="shortcut icon" type="image/x-icon"  href="/icon_uf.png" >
+                <!-- Bootstrap CSS -->
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>
+                <link rel="stylesheet" type="text/css" href="style.css"/>
+                <title>Rage Against The aPi</title>
+                <!-- Optional JavaScript -->
+                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
+                <script type="text/javascript" src="script.js"></script>
+        </head>
+        <body>
 <div class="container">
 <h1 class="form-heading">Rage Against The aPi</h1>
 <div class="login-form">
@@ -27,7 +27,7 @@
    <h2></h2>
    <p>Please enter place</p>
    </div>
-    <span id="Login" class='row'>
+    <span id="Login" class='container row'>
         <div class="form-group col-md-6" id='src1'>
             <input type="text" class="form-control" id="inputStart" placeholder="Start Place">
         </div>
@@ -35,53 +35,16 @@
             <input type="text" class="form-control" id="inputLast" placeholder="Finish Place">
         </div>
         <button type="submit" id='searchStation' href="#" class="btn btn-danger">SEARCH PLACE</button>
-        <div id='first-last'>
-	        <div class="form-group col-md-6" id='src2'>
-	        	<hr>
-				<label class="radio">Place1
-					<input type="radio" checked="checked" name="is_company">
-					<span class="checkround"></span>
-				</label>
-				<label class="radio">Place2
-					<input type="radio" name="is_company">
-					<span class="checkround"></span>
-				</label>
-				<label class="radio">Place3
-					<input type="radio" name="is_company">
-					<span class="checkround"></span>
-				</label>
-				<label class="radio">Place4
-					<input type="radio" name="is_company">
-					<span class="checkround"></span>
-				</label>
-	        </div>
-	        <div class="form-group col-md-6" id='dst2'>
-	        	<hr>
-				<label class="radio">Place5
-					<input type="radio" checked="checked" name="is_company2">
-					<span class="checkround"></span>
-				</label>
-				<label class="radio">Place6
-					<input type="radio" name="is_company2">
-					<span class="checkround"></span>
-				</label>
-				<label class="radio">Place7
-					<input type="radio" name="is_company2">
-					<span class="checkround"></span>
-				</label>
-				<label class="radio">Place8
-					<input type="radio" name="is_company2">
-					<span class="checkround"></span>
-				</label>
-	        </div>
-    	</div>
+        <div class='col-md-12 row' id='first-last'>
+
+        </div>
         <button type="submit" class="btn btn-primary">SELECT PlACE</button>
     </span>
     </div>
 <p class="botto-text">apakhomo, mgayduk &copy; 2018</p>
 <script>
   $(document).on('click', '#searchStation', function(e){
-  	console.log('click');
+    console.log('click');
     $.ajax({
         type:"POST",
         url: '/start.php',
@@ -90,7 +53,7 @@
          last: $( "#inputLast" ).val(),
       },
         success: function(response){
-        	console.log(response);
+            console.log(response);
           $('#first-last').html(response);
       }
     });
--- app/start.php
+++ app/start.php
@@ -2,17 +2,31 @@
 
 require_once('RatpClient.php'); 
 
-if (isset($_POST['first']) && isset($_POST['last']))
+if (isset($_POST['first']) && isset($_POST['last']) && strlen($_POST['first']) && strlen($_POST['last']) )
 {
 	$c = new RatpClient();
 	$r1 = $c->getMetro($_POST['first']);
-	echo "<pre>";
-	print_r($r1);
-	echo "</pre>";
-
+	// echo "<pre>";
+	// print_r($r1);
+	// echo "</pre>";
+	?><div class="form-group col-md-6" id='src2'><hr><?php
+	foreach ($r1 as $key) { ?>
+        <label class="radio"><?php echo $key['label']; ?>
+            <input type="radio" checked="checked" name="is_company">
+            <span class="checkround"></span>
+        </label>
+<?php }
+	?></div><?php
 	$r2 = $c->getMetro($_POST['last']);
-	echo "<pre>";
-	print_r($r2);
-	echo "</pre>";
+	// echo "<pre>";
+	// print_r($r2);
+	// echo "</pre>";
 	
-}
+}	?><div class="form-group col-md-6" id='dst2'><hr><?php
+	foreach ($r2 as $key) { ?>
+        <label class="radio"><?php echo $key['label']; ?>
+            <input type="radio" checked="checked" name="is_company2">
+            <span class="checkround"></span>
+        </label>
+<?php }
+	?></div>
