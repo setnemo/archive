7a6988b2665fc6a26fe8cc1382746f0e18cec470
Artem Pakhomov <setnemo@gmail.com>
2018-10-28 23:39:59 +0200

    ..

--- app/second.php
+++ app/second.php
@@ -10,26 +10,9 @@ $routes = $c->getRoutes();
 
 $jf = new JourneyFinder();
 
-
-// echo "<pre>";
-// print_r($journey);
-// echo "</pre>";
-// https://api.navitia.io/v1/coverage/sandbox/networks/network%3ARAT%3A1/routes?depth=3&
-
-
-if (isset($_POST['first']) && isset($_POST['last']) && strlen($_POST['first']) > 0 && strlen($_POST['last']) > 0 )
-{
 $journey = $jf->getJourney($routes, $_POST['first'], $_POST['last']);
-	?>
 
-
-	<div ><hr><?php
-	if (count($$journey) == 0 ) { exit ;}
-	foreach ($journey as $key) { ?>
-        <?php echo $key['label']; ?><br>
-
-<?php }
-	?>
-
-	</div>
-
+echo "<pre>";
+print_r($journey);
+echo "</pre>";
+// https://api.navitia.io/v1/coverage/sandbox/networks/network%3ARAT%3A1/routes?depth=3&
