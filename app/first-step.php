d4b8c026499c28737b1f360580aa2b226e164914
Artem Pakhomov <setnemo@gmail.com>
2018-10-28 12:22:39 +0200

    new

--- app/first-step.php
+++ app/first-step.php
@@ -4,7 +4,12 @@ require_once('RatpClient.php');
 
 
 $c = new RatpClient();
-$r = $c->getPlace('gare');
+$r = $c->getMetro('gare');
+// echo "<pre>";
+// print_r($r);
+// echo "</pre>";
+
+$r = $c->getTest('stop_area:OIF:SA:8768600');
 echo "<pre>";
 print_r($r);
 echo "</pre>";
